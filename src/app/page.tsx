1cd68e6b4ccb998320b9643e310c711a87ed7f61
Anil Bhatt <67644691+anilbhatt094@users.noreply.github.com>
2024-04-29 10:52:16 +0000

    AnalyticsandSpeedLight

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import Header from '../component/Header';
 import Footer from '../component/Footer';
 import Home from '../pages/Home';
 function page() {
   return (
     <>
-
+<Analytics/>
+<SpeedInsights/>
   <Header/>
     <Home/>
   <Footer/>
